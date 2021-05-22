d7da4cc142883d50c28e3560dd9c212add6a4879
asar <asar.bs@gmail.com>
2021-05-22 22:40:02 +0200

    init commit

---
+++ src/GameMain.h
@@ -0,0 +1,16 @@
+#ifndef GAMEMAIN_H_INCLUDED
+#define GAMEMAIN_H_INCLUDED
+
+#include <SFML/Graphics.hpp>
+
+class GameMain
+{
+    public:
+        GameMain();
+        void mainGameLoop();
+    protected:
+    private:
+
+};
+
+#endif // GAMEMAIN_H_INCLUDED
